205efe710437ea73438c0ca61fbee186c5dc06ba
Arjun Vasavan <vasavanarjun@gmail.com>
2026-01-05 08:54:35 +0530

    added bug handling on prompt printing

--- signals.c
+++ signals.c
@@ -9,22 +9,26 @@ int signal_details_index = 0;
 void signal_handler(int signum) {
     extern char prompt[25];
     extern pid_t pid;
-    
+
     if (signum == SIGINT) { // ctrl+c
         if (pid == 0) {
-            printf("\033[1;36m");
-            printf("\n%s\n", prompt);
-            printf("\033[01;33m");
-            printf("$ ");
-            printf("\033[0m");
+            printf(
+                "\n\033[1;36m%s\n"
+                "\033[01;33m$ "
+                "\033[0m",
+                prompt
+            );
+            fflush(stdout);
         }
     } else if (signum == SIGTSTP) { // ctrl+z
         if (pid == 0) { // at child process
-            printf("\033[1;36m");
-            printf("\n%s\n", prompt);
-            printf("\033[01;33m");
-            printf("$ ");
-            printf("\033[0m");
+            printf(
+                "\n\033[1;36m%s\n"
+                "\033[01;33m$ "
+                "\033[0m",
+                prompt
+            );
+            fflush(stdout);
         } else { // during parent process ctrl+z is called
             extern char input_string[25];
             signal_details[signal_details_index].spid = pid;
@@ -34,7 +38,7 @@ void signal_handler(int signum) {
     } else if (signum == SIGCHLD) {
         int status;
         pid_t terminated_pid = waitpid(-1, &status, WNOHANG);
-        
+
         if (terminated_pid > 0) {
             // Find and remove the terminated process from array
             for (int i = 0; i < signal_details_index; i++) {
